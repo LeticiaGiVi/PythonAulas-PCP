f60ba78bfaacf8be33725cd38ed3c01154e42df3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 22:36:29 -0300

    aula

---
+++ pythonProject/Aula9/APP.py
@@ -0,0 +1,36 @@
+from stages import lead_model
+import repo
+def main():
+    while True:
+        print_menu()
+        op = input("Escolha: ")
+        if op == "1":
+            add_lead()
+        elif op == "2":
+            list_leads()
+        elif op == "2":
+            print("Ate logo")
+            break
+        else:
+            print("opção invalida")
+            break
+
+
+def add_lead():
+    name = input("Nome: ")
+    company = input("empresa: ")
+    email = input("e-mail: ")
+    print(lead_model(name, company, email))
+    print("Lead adicionado")
+
+def list_leads():
+    print("listar Lead")
+
+def print_menu():
+    print("\n Mini CRM de Leads - (Adicionar/Listar)")
+    print("[1]Adicionar Lead")
+    print(" [2]Listar leads")
+    print("[0]Sair")
+
+if __name__ == "__main__":
+    main()
